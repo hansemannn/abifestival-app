6b0273f005f939b864954ce5dbd376218202ecc0
Hans Knöchel <hansknoechel@gmail.com>
2017-06-23 23:33:07 +0200

    Add last settings, add social networks

--- app/controllers/news.js
+++ app/controllers/news.js
@@ -1,5 +1,9 @@
 var api = require('/api');
 var moment = require('alloy/moment');
+var Social = {
+	Facebook: 'Facebook',
+	Instagram: 'Instagram'
+};
 
 (function constructor(args) {
 	
@@ -34,4 +38,35 @@ function loadNews() {
 function openNews(e) {
 	var news = e.itemId;		
 	Alloy.Globals.tabGroup.activeTab.openWindow(Alloy.createController('/newsDetails', { news: news }).getView());
+}
+
+function openInstagram() {
+	openSocialAccount(Alloy.CFG.festival.socials.instagram, Social.Instagram, 'instagram://user?username=', 'http://instagram.com');
+}
+
+function openFacebook() {
+	openSocialAccount(Alloy.CFG.festival.socials.facebook, Social.Facebook, 'fb://profile/', 'https://facebook.com');
+}
+
+function openSocialAccount(username, socialNetwork, urlScheme, homepage) {
+	var name = Alloy.CFG.festival.name;
+	
+	var dia = Ti.UI.createAlertDialog({
+		title: name + ' auf ' + socialNetwork,
+		message: 'Möchtest du jetzt zum ' + name + ' auf ' + socialNetwork + ' weitergeleitet werden?',
+		buttonNames: ['Abbrechen', 'Öffnen'],
+		cancel: 0
+	});
+	
+	dia.addEventListener('click', function(e) {
+		if (e.index === 1) {
+			if (Ti.Platform.canOpenURL(urlScheme.split('://')[0] + '://')) {
+				Ti.Platform.openURL(urlScheme + username);
+			} else {
+				Ti.Platform.openURL(homepage + '/' + username);
+			}
+		}
+	});
+	
+	dia.show();
 }
