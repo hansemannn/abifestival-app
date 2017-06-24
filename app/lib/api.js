71d64a2992a16df190dd37ebd121c28bad6f4c46
Hans Knöchel <hansknoechel@gmail.com>
2017-06-24 20:27:25 +0200

    Use static JSON files

--- app/lib/api.js
+++ app/lib/api.js
@@ -1,6 +1,19 @@
 var Request = require('/request');
+var isDemo = require('auth').isDemo();
+
+function loadLocalJSON(identifier) {
+	return JSON.parse(Ti.Filesystem.getFile(Ti.Filesystem.getResourcesDirectory(), 'json/' + identifier + '.json').read());
+}
 
 exports.getNews = function(cb, error) {
+	if (isDemo === true) {
+		setTimeout(function() {
+			cb(loadLocalJSON('news'));
+		}, 1000);
+		
+		return;
+	}
+	
 	var request = new Request({
 		url : '/news/list?festival=' + Alloy.CFG.festival.id,
 		type : 'GET',
@@ -15,6 +28,14 @@ exports.getNews = function(cb, error) {
 };
 
 exports.getInfos = function(cb, error) {
+	if (isDemo === true) {
+		setTimeout(function() {
+			cb(loadLocalJSON('infos'));
+		}, 1000);
+		
+		return;
+	}
+	
 	var request = new Request({
 		url : '/infopages/' + Alloy.CFG.festival.id + '/index.php',
 		type : 'GET',
@@ -29,6 +50,14 @@ exports.getInfos = function(cb, error) {
 };
 
 exports.getBands = function(cb, error) {
+	if (isDemo === true) {
+		setTimeout(function() {
+			cb(loadLocalJSON('bands'));
+		}, 1000);
+		
+		return;
+	}
+	
 	var request = new Request({
 		url : '/band/list?festival=' + Alloy.CFG.festival.id,
 		type : 'GET',
